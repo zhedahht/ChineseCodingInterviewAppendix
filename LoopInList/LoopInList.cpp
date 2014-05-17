a2e6d68220e5d4e20151414863337d290d8ccbbe
Harry He <zhedahht@hotmail.com>
2014-05-17 11:41:39 +0800

    Add project HasLoop

---
+++ LoopInList/LoopInList.cpp
@@ -0,0 +1,186 @@
+#include <stdio.h>
+#include "../Utilities/list.h"
+
+bool HasLoop(ListNode* pHead)
+{
+    if(pHead == NULL)
+        return false;
+
+    ListNode* pSlow = pHead->m_pNext;
+    if(pSlow == NULL)
+        return false;
+
+    ListNode* pFast = pSlow->m_pNext;
+    while(pFast != NULL && pSlow != NULL)
+    {
+        if(pFast == pSlow)
+            return true;
+
+        pSlow = pSlow->m_pNext;
+
+        pFast = pFast->m_pNext;
+        if(pFast != NULL)
+            pFast = pFast->m_pNext;
+    }
+
+    return false;
+}
+
+// ==================== Test Code ====================
+void Test(char* testName, ListNode* pHead, bool expected)
+{
+    if(testName != NULL)
+        printf("%s begins: ", testName);
+
+    if(HasLoop(pHead) == expected)
+        printf("HasLoop Passed.\n");
+    else
+        printf("HasLoop FAILED.\n");
+}
+
+// A list has a node, without a loop
+void Test1()
+{
+    ListNode* pNode1 = CreateListNode(1);
+
+    Test("Test1", pNode1, false);
+
+    DestroyList(pNode1);
+}
+
+// A list has a node, with a loop
+void Test2()
+{
+    ListNode* pNode1 = CreateListNode(1);
+    ConnectListNodes(pNode1, pNode1);
+
+    Test("Test2", pNode1, true);
+
+    delete pNode1;
+    pNode1 = NULL;
+}
+
+// A list has multiple nodes, with a loop 
+void Test3()
+{
+    ListNode* pNode1 = CreateListNode(1);
+    ListNode* pNode2 = CreateListNode(2);
+    ListNode* pNode3 = CreateListNode(3);
+    ListNode* pNode4 = CreateListNode(4);
+    ListNode* pNode5 = CreateListNode(5);
+
+    ConnectListNodes(pNode1, pNode2);
+    ConnectListNodes(pNode2, pNode3);
+    ConnectListNodes(pNode3, pNode4);
+    ConnectListNodes(pNode4, pNode5);
+    ConnectListNodes(pNode5, pNode3);
+
+    Test("Test3", pNode1, true);
+
+    delete pNode1;
+    pNode1 = NULL;
+    delete pNode2;
+    pNode2 = NULL;
+    delete pNode3;
+    pNode3 = NULL;
+    delete pNode4;
+    pNode4 = NULL;
+    delete pNode5;
+    pNode5 = NULL;
+}
+
+// A list has multiple nodes, with a loop 
+void Test4()
+{
+    ListNode* pNode1 = CreateListNode(1);
+    ListNode* pNode2 = CreateListNode(2);
+    ListNode* pNode3 = CreateListNode(3);
+    ListNode* pNode4 = CreateListNode(4);
+    ListNode* pNode5 = CreateListNode(5);
+
+    ConnectListNodes(pNode1, pNode2);
+    ConnectListNodes(pNode2, pNode3);
+    ConnectListNodes(pNode3, pNode4);
+    ConnectListNodes(pNode4, pNode5);
+    ConnectListNodes(pNode5, pNode1);
+
+    Test("Test4", pNode1, true);
+
+    delete pNode1;
+    pNode1 = NULL;
+    delete pNode2;
+    pNode2 = NULL;
+    delete pNode3;
+    pNode3 = NULL;
+    delete pNode4;
+    pNode4 = NULL;
+    delete pNode5;
+    pNode5 = NULL;
+}
+
+// A list has multiple nodes, with a loop 
+void Test5()
+{
+    ListNode* pNode1 = CreateListNode(1);
+    ListNode* pNode2 = CreateListNode(2);
+    ListNode* pNode3 = CreateListNode(3);
+    ListNode* pNode4 = CreateListNode(4);
+    ListNode* pNode5 = CreateListNode(5);
+
+    ConnectListNodes(pNode1, pNode2);
+    ConnectListNodes(pNode2, pNode3);
+    ConnectListNodes(pNode3, pNode4);
+    ConnectListNodes(pNode4, pNode5);
+    ConnectListNodes(pNode5, pNode5);
+
+    Test("Test5", pNode1, true);
+
+    delete pNode1;
+    pNode1 = NULL;
+    delete pNode2;
+    pNode2 = NULL;
+    delete pNode3;
+    pNode3 = NULL;
+    delete pNode4;
+    pNode4 = NULL;
+    delete pNode5;
+    pNode5 = NULL;
+}
+
+// A list has multiple nodes, without a loop 
+void Test6()
+{
+    ListNode* pNode1 = CreateListNode(1);
+    ListNode* pNode2 = CreateListNode(2);
+    ListNode* pNode3 = CreateListNode(3);
+    ListNode* pNode4 = CreateListNode(4);
+    ListNode* pNode5 = CreateListNode(5);
+
+    ConnectListNodes(pNode1, pNode2);
+    ConnectListNodes(pNode2, pNode3);
+    ConnectListNodes(pNode3, pNode4);
+    ConnectListNodes(pNode4, pNode5);
+
+    Test("Test6", pNode1, false);
+
+    DestroyList(pNode1);
+}
+
+// Empty list
+void Test7()
+{
+    Test("Test7", NULL, false);
+}
+
+int main(int argc, char* argv[])
+{
+    Test1();
+    Test2();
+    Test3();
+    Test4();
+    Test5();
+    Test6();
+    Test7();
+
+	return 0;
+}
