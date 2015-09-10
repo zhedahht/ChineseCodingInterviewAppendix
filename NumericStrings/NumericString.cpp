18bdb32f10d5ba586a0b09b381110ae468859a8e
Harry He <zhedahht@hotmail.com>
2015-09-10 16:26:00 -0700

    Format Code
    Rename parameter string as str in order to void potential name conflicts, because string is a type name in C++ STL.
    
    Test Cases added in the previous change:
        Test("Test16", ".", false);
        Test("Test17", ".e1", false);
        Test("Test18", "+.", false);

--- NumericStrings/NumericString.cpp
+++ NumericStrings/NumericString.cpp
@@ -7,112 +7,111 @@
 
 #include <stdio.h>
 
-bool scanDigits(char** string);
-bool isExponential(char** string);
+bool scanDigits(char** str);
+bool isExponential(char** str);
 
-bool isNumeric(char* string)
+bool isNumeric(char* str)
 {
-   if (string == NULL)
-      return false;
+    if (str == NULL)
+        return false;
 
-   if (*string == '+' || *string == '-')
-      ++string;
-   if (*string == '\0')
-      return false;
+    if (*str == '+' || *str == '-')
+        ++str;
+    if (*str == '\0')
+        return false;
 
-   bool numeric = true;
+    bool numeric = true;
 
-   bool hasDigits = scanDigits(&string);
-   if (*string != '\0')
-   {
-      // for floats
-      if (*string == '.')
-      {
-         ++string;
-         if (*string == 'e' || *string == 'E')
-            return false;
-         if (!hasDigits && *string == '\0')
-            return false;
+    bool hasDigits = scanDigits(&str);
+    if (*str != '\0')
+    {
+        // for floats
+        if (*str == '.')
+        {
+            ++str;
+            if (*str == 'e' || *str == 'E')
+                return false;
+            if (!hasDigits && *str == '\0')
+                return false;
 
-         scanDigits(&string);
+            scanDigits(&str);
 
-         if (*string == 'e' || *string == 'E')
-            numeric = isExponential(&string);
-      }
-      // for integers
-      else if (*string == 'e' || *string == 'E')
-         numeric = isExponential(&string);
-      else
-         numeric = false;
-   }
+            if (*str == 'e' || *str == 'E')
+                numeric = isExponential(&str);
+        }
+        // for integers
+        else if (*str == 'e' || *str == 'E')
+            numeric = isExponential(&str);
+        else
+            numeric = false;
+    }
 
-   return numeric && *string == '\0';
+    return numeric && *str == '\0';
 }
 
-bool scanDigits(char** string)
+bool scanDigits(char** str)
 {
-   char* pBefore = *string;
+    char* pBefore = *str;
 
-   while (**string != '\0' && **string >= '0' && **string <= '9')
-      ++(*string);
+    while (**str != '\0' && **str >= '0' && **str <= '9')
+        ++(*str);
 
-   // return true when there are some digits in string
-   return *string > pBefore;
+    // return true when there are some digits in str
+    return *str > pBefore;
 }
 
-bool isExponential(char** string)
+bool isExponential(char** str)
 {
-   if (**string != 'e' && **string != 'E')
-      return false;
+    if (**str != 'e' && **str != 'E')
+        return false;
 
-   ++(*string);
-   if (**string == '+' || **string == '-')
-      ++(*string);
+    ++(*str);
+    if (**str == '+' || **str == '-')
+        ++(*str);
 
-   if (**string == '\0')
-      return false;
+    if (**str == '\0')
+        return false;
 
-   scanDigits(string);
-   return (**string == '\0') ? true : false;
+    scanDigits(str);
+    return (**str == '\0') ? true : false;
 }
 
 // ==================== Test Code ====================
 
-void Test(char* testName, char* string, bool expected)
+void Test(char* testName, char* str, bool expected)
 {
-   if (testName != NULL)
-      printf("%s begins: ", testName);
+    if (testName != NULL)
+        printf("%s begins: ", testName);
 
-   if (isNumeric(string) == expected)
-      printf("Passed.\n");
-   else
-      printf("FAILED.\n");
+    if (isNumeric(str) == expected)
+        printf("Passed.\n");
+    else
+        printf("FAILED.\n");
 }
 
 int main(int argc, char* argv[])
 {
-   Test("Test1", "100", true);
-   Test("Test2", "123.45e+6", true);
-   Test("Test3", "+500", true);
-   Test("Test4", "5e2", true);
-   Test("Test5", "3.1416", true);
-   Test("Test6", "600.", true);
-   Test("Test7", "-.123", true);
-   Test("Test8", "-1E-16", true);
-   Test("Test9", "1.79769313486232E+308", true);
-   Test("Test10", "179.", true);
+    Test("Test1", "100", true);
+    Test("Test2", "123.45e+6", true);
+    Test("Test3", "+500", true);
+    Test("Test4", "5e2", true);
+    Test("Test5", "3.1416", true);
+    Test("Test6", "600.", true);
+    Test("Test7", "-.123", true);
+    Test("Test8", "-1E-16", true);
+    Test("Test9", "1.79769313486232E+308", true);
 
-   printf("\n\n");
+    printf("\n\n");
 
-   Test("Test10", "12e", false);
-   Test("Test11", "1a3.14", false);
-   Test("Test12", "1+23", false);
-   Test("Test13", "1.2.3", false);
-   Test("Test14", "+-5", false);
-   Test("Test15", "12e+5.4", false);
-   Test("Test16", ".", false);
-   Test("Test17", ".e1", false);
-   Test("Test18", "+.", false);
+    Test("Test10", "12e", false);
+    Test("Test11", "1a3.14", false);
+    Test("Test12", "1+23", false);
+    Test("Test13", "1.2.3", false);
+    Test("Test14", "+-5", false);
+    Test("Test15", "12e+5.4", false);
+    Test("Test16", ".", false);
+    Test("Test17", ".e1", false);
+    Test("Test18", "+.", false);
 
-   return 0;
+    return 0;
 }
